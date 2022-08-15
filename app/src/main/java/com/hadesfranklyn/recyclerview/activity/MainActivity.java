11bbae17446722312e66fd8f7c896e685d7f4106
Franklyn R <franklyn1998@live.com>
2022-08-15 16:25:43 -0300

    RecyclerView - parte 3

--- app/src/main/java/com/hadesfranklyn/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/recyclerview/activity/MainActivity.java
@@ -7,10 +7,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hadesfranklyn.recyclerview.R;
+import com.hadesfranklyn.recyclerview.adapter.Adapter;
+import com.hadesfranklyn.recyclerview.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +25,33 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        //Listagem de filmes
+        this.criarFilmes();
+
         //Configurar adapter
+        Adapter adapter = new Adapter();
 
         //Configurar RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-//        recyclerView.setAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void criarFilmes() {
+        Filme filme = new Filme("O Predador: A Caçada", "Ficção científica/Ação", "2022");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Agente Oculto", "Ação/Suspense", "2022");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Tudo em Todo o Lugar ao Mesmo Tempo", "Aventura/Ficção científica", "2022");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Cidade Perdida", "Romance/Aventura", "2022");
+        this.listaFilmes.add(filme);
+
+        filme = new Filme("Lightyear", "Aventura/Infantil", "2022");
+        this.listaFilmes.add(filme);
     }
 }
--- app/src/main/java/com/hadesfranklyn/recyclerview/adapter/Adapter.java
+++ app/src/main/java/com/hadesfranklyn/recyclerview/adapter/Adapter.java
@@ -1,5 +1,6 @@
 package com.hadesfranklyn.recyclerview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,25 +8,33 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.hadesfranklyn.recyclerview.R;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_lista, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.titulo.setText("Título de teste");
+        holder.genero.setText("Comédia");
+        holder.ano.setText("2022");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView titulo;
         TextView ano;
@@ -33,6 +42,11 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            ano = itemView.findViewById(R.id.textAno);
+            genero = itemView.findViewById(R.id.textGenero);
+
         }
     }
 }
