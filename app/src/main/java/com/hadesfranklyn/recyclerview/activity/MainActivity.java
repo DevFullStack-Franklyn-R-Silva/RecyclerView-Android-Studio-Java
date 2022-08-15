fcfa63265994acd5a81c6965bf9650cef9ee6d6b
Franklyn R <franklyn1998@live.com>
2022-08-15 15:59:26 -0300

    RecyclerView - parte 2

--- app/src/main/java/com/hadesfranklyn/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/recyclerview/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.hadesfranklyn.recyclerview;
+package com.hadesfranklyn.recyclerview.activity;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.hadesfranklyn.recyclerview.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/hadesfranklyn/recyclerview/adapter/Adapter.java
@@ -0,0 +1,38 @@
+package com.hadesfranklyn.recyclerview.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titulo;
+        TextView ano;
+        TextView genero;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
