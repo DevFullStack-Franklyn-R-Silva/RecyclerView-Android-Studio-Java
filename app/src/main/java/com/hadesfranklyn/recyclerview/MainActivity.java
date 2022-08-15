a5fe775b0ef717a0726ea0563ee5cc620cb8b4df
Franklyn R <franklyn1998@live.com>
2022-08-15 15:42:50 -0300

    RecyclerView - parte 1

---
+++ app/src/main/java/com/hadesfranklyn/recyclerview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hadesfranklyn.recyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar adapter
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.setAdapter();
+    }
+}
